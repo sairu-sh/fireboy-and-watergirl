54176a00b83adedb2924980d2c29d92314f86b7f
sairu-sh <tmg.sairush@gmail.com>
2023-12-12 15:30:20 +0545

    added utility code. Tracks fireboy movement, watergirl movement, eventListeners for keypress and release, and function to detect collision

---
+++ game/src/js/utils.js
@@ -0,0 +1,54 @@
+const fireboyMovement = {
+  right: false,
+  left: false,
+  isGrounded: true,
+};
+
+const watergirlMovement = {
+  right: false,
+  left: false,
+  isGrounded: true,
+};
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowRight":
+      fireboyMovement.right = true;
+      break;
+    case "ArrowLeft":
+      fireboyMovement.left = true;
+      break;
+    case "d":
+      watergirlMovement.right = true;
+      break;
+    case "a":
+      watergirlMovement.left = true;
+      break;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "ArrowRight":
+      fireboyMovement.right = false;
+      break;
+    case "ArrowLeft":
+      fireboyMovement.left = false;
+      break;
+    case "d":
+      watergirlMovement.right = false;
+      break;
+    case "a":
+      watergirlMovement.left = false;
+      break;
+  }
+});
+
+function detectCollision({ object1, object2 }) {
+  return (
+    object1.position.y + object1.height >= object2.position.y &&
+    object1.position.x <= object2.position.x + object2.width &&
+    object1.position.x + object1.width >= object2.position.x &&
+    object1.position.y <= object2.position.y + object2.height
+  );
+}
